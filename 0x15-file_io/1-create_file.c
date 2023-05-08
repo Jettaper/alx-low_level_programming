558d4654bcde7166f6ac5967c3c52bdbfa0e58ff
Jettaper <saade9887@gmail.com>
2023-05-08 13:29:39 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - this script creates a file
+ * create_file - this script creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
@@ -31,5 +31,4 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
- 
 
